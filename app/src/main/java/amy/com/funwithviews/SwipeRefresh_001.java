d5916f6fc546ab9617cf3641f14aa36ccc0c9821
NatsumeReiko <sprix.reiko.natsume@gmail.com>
2016-05-20 19:34:15 +0900

    Initial commit

---
+++ app/src/main/java/amy/com/funwithviews/SwipeRefresh_001.java
@@ -0,0 +1,13 @@
+package amy.com.funwithviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwipeRefresh_001 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_refresh_001);
+    }
+}
